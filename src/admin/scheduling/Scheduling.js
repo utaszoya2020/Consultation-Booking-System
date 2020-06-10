94cb270d156a94849d9a421d0cc7dfdd2202b1e5
Xiexuc <liachenxiexu@gmail.com>
2020-06-10 18:22:34 +1000

    remove unused code

--- src/admin/scheduling/Scheduling.js
+++ src/admin/scheduling/Scheduling.js
@@ -104,24 +104,12 @@ class Scheduling extends Component {
         
     } */
 
-    handleChange = (nextTargetKeys, direction, moveKeys) => {
+    handleChange = (nextTargetKeys) => {
         this.setState({ currentSessionTime: nextTargetKeys });
-    
-        //console.log('targetKeys: ', nextTargetKeys);
-        //console.log('direction: ', direction);
-        //console.log('moveKeys: ', moveKeys);
     };
     
     handleSelectChange = (sourceSelectedKeys, targetSelectedKeys) => {
         this.setState({ selectedKeys: [...sourceSelectedKeys, ...targetSelectedKeys] });
-    
-        //console.log('sourceSelectedKeys: ', sourceSelectedKeys);
-        //console.log('targetSelectedKeys: ', targetSelectedKeys);
-    };
-    
-    handleScroll = (direction, e) => {
-        //console.log('direction:', direction);
-        //console.log('target:', e.target);
     };
 
     handleUpdate = () => {
@@ -197,7 +185,6 @@ class Scheduling extends Component {
                                         selectedKeys={selectedKeys}
                                         onChange={this.handleChange}
                                         onSelectChange={this.handleSelectChange}
-                                        onScroll={this.handleScroll}
                                         render={item => item.title}
                                         oneWay={y}
                                         listStyle={{
