6f05f1df1b6acc29e486834572e0c5516c8e474c
Jessie Hou <jessie.houjinzhi@gmail.com>
2020-10-05 00:44:22 +1100

    production env

--- src/constants/env.js
+++ src/constants/env.js
@@ -1,4 +1,4 @@
-const BASE_URL = 'http://localhost:3000/api/'
-//process.env.NODE_ENV === 'production' ? 'http://aibt-env.eba-8j4gbazh.us-east-2.elasticbeanstalk.com/api/' : 'http://localhost:3000/api';
+const BASE_URL = 
+process.env.NODE_ENV === 'production' ? 'http://aibt-env.eba-8j4gbazh.us-east-2.elasticbeanstalk.com/api/' : 'http://localhost:3000/api';
 
 export default BASE_URL;
